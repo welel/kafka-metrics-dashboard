e1c5b8df456b71b2c342f39bf4339257cad1fe02
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-09-18 19:08:55 +0300

    feat: refreshing data

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,6 +5,9 @@ LABELS = {
     'tokopedia_3': 'Tokopedia Sellers',
 }
 
+var tasksLineCharts = {};
+var progressPieCharts = {};
+
 
 function formatToK(number) {
     let k_num = 0;
@@ -92,10 +95,10 @@ function fillGraphTasksData(offset_name, graph_data) {
 
     let ctx = document.getElementById(`tasksChart-${offset_name}`);
 
-    new Chart(ctx, {
+    tasksLineCharts[offset_name] = new Chart(ctx, {
         type: 'line',
         data: chartData
-    })
+    });
 }
 
 
@@ -116,10 +119,28 @@ function fillGraphProgressData(offset_name, progress) {
 
     let ctx = document.getElementById(`progressPieChart-${offset_name}`);
 
-    new Chart(ctx, {
+    progressPieCharts[offset_name] = new Chart(ctx, {
         type: 'pie',
         data: chartData
     })
+}
+
+
+function refreshGraphTasksData(offset_name, graph_data) {
+    chart = tasksLineCharts[offset_name];
+    chart.data.labels = graph_data.labels;
+    chart.data.datasets[0].data = graph_data.lines.processed;
+    chart.data.datasets[1].data = graph_data.lines.queued;
+    chart.data.datasets[2].data = graph_data.lines.total;
+    chart.update();
+}
+
+
+function refreshGraphProgressData(offset_name, progress) {
+    let percent = progress || 0;
+    chart = progressPieCharts[offset_name];
+    chart.data.datasets[0].data = [percent, 100 - percent];
+    chart.update();
 }
 
 
@@ -167,9 +188,9 @@ function createTopicsAccordion(offsets) {
         }  
 
         let topicAccordionItem = `
-            <div class="accordion-item">
+            <div class="accordion-item ${offset_name}">
             <h2 class="accordion-header" id="panelsStayOpen-heading${offset_name}">
-            <button class="accordion-button accordion-${status}-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${offset_name}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${offset_name}">
+            <button class="accordion-button ${status} collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${offset_name}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${offset_name}">
                 ${status_icon} ${label} | ${progress}%
             </button>
             </h2>
@@ -179,7 +200,7 @@ function createTopicsAccordion(offsets) {
                     <div class="col-12 col-md-4 col-xl-3 topic-top-card">
                         <div class="py-3">
                         <div class="row m-0 px-2">
-                            <div class="col-9 fs-2 topic-tasks-number">${total}</div>
+                            <div class="col-9 fs-2 topic-tasks-number topic-tasks-total">${total}</div>
                             <div class="col-3 mt-2"><i class="bi bi-list-nested" style="font-size: 1rem; color:#407eff;"></i></div>
                         </div>
                         <div class="row m-0 fs-6 text-secondary ps-3">Total</div>
@@ -188,7 +209,7 @@ function createTopicsAccordion(offsets) {
                     <div class="col-12 col-md-4 col-xl-3 topic-top-card">
                         <div class="py-3">
                         <div class="row m-0 px-2">
-                            <div class="col-9 fs-2 topic-tasks-number">${processed}</div>
+                            <div class="col-9 fs-2 topic-tasks-number  topic-tasks-processed">${processed}</div>
                             <div class="col-3 mt-2"><i class="bi bi-list-check" style="font-size: 1rem; color:lightgreen;"></i></div>
                         </div>
                         <div class="row m-0 fs-6 text-secondary ps-3">Processed</div>
@@ -197,7 +218,7 @@ function createTopicsAccordion(offsets) {
                     <div class="col-12 col-md-4 col-xl-3 topic-top-card">
                         <div class="py-3">
                         <div class="row m-0 px-2">
-                            <div class="col-9 fs-2 topic-tasks-number">${remaining}</div>
+                            <div class="col-9 fs-2 topic-tasks-number topic-tasks-queued">${remaining}</div>
                             <div class="col-3 mt-2"><i class="bi bi-list-ul" style="font-size: 1rem; color: gold;"></i></div>
                         </div>
                         <div class="row m-0 fs-6 text-secondary ps-3">Queued</div>
@@ -209,7 +230,7 @@ function createTopicsAccordion(offsets) {
                         <div class="col-4 col-lg-6 topic-top-card">
                             <div class="py-3">
                             <div class="row m-0 px-2">
-                                <div class="col-9 fs-4 fw-bold">${load_speed}<span class="fs-5 text-secondary fw-normal">/s</span></div>
+                                <div class="col-9 fs-4 fw-bold"><span class="topic-speed-load">${load_speed}</span><span class="fs-5 text-secondary fw-normal">/s</span></div>
                             </div>
                             <div class="row m-0 fs-6 text-secondary ps-3">Load speed</div>
                             </div>
@@ -217,7 +238,7 @@ function createTopicsAccordion(offsets) {
                         <div class="col-4 col-lg-6 topic-top-card">
                             <div class="py-3">
                             <div class="row m-0 px-2">
-                                <div class="col-9 fs-4 fw-bold">${processing_speed}<span class="fs-5 text-secondary fw-normal">/s</span></div>
+                                <div class="col-9 fs-4 fw-bold"><span class="topic-speed-processing">${processing_speed}</span><span class="fs-5 text-secondary fw-normal">/s</span></div>
                             </div>
                             <div class="row m-0 fs-6 text-secondary ps-3">Proc. speed</div>
                             </div>
@@ -226,9 +247,9 @@ function createTopicsAccordion(offsets) {
                         <div class="col-4 d-block d-lg-none topic-top-card">
                             <div class="py-3">
                             <div class="row m-0 px-2">
-                                <div class="col-9 fs-4 fw-bold">${progress}%</div>
+                                <div class="col-9 fs-4 fw-bold"><span class="topic-progress">${progress}</span>%</div>
                             </div>
-                            <div class="row m-0 fs-6 text-secondary ps-3">Left ${time_left}</div>
+                            <div class="row m-0 fs-6 text-secondary ps-3"><span>Left <span class="topic-lime-left">${time_left}</span></span></div>
                             </div>
                         </div>
                         </dev>
@@ -247,12 +268,12 @@ function createTopicsAccordion(offsets) {
                         <span class="fs-6 fw-500">Time left</span>
                     </div>
                     <div class="row text-center">
-                        <span class="fs-5">${time_left}</span>
+                        <span class="fs-5 topic-lime-left">${time_left}</span>
                     </div>
                     <hr>
                     <div class="row text-center">
-                        <div class="col-6"><span class="fw-500">Started</span><br>${started}</div>
-                        <div class="col-6"><span class="fw-500">Finishes</span><br>${finishes}</div>
+                        <div class="col-6"><span class="fw-500">Started</span><br><span class="topic-time-started">${started}</span></div>
+                        <div class="col-6"><span class="fw-500">Finishes</span><br><span class="topic-time-finishes">${finishes}</span></div>
                     </div>
                     </div>
                     <div class="col-12 col-lg-9">
@@ -278,6 +299,69 @@ function createTopicsAccordion(offsets) {
 }
 
 
+function fillTopicsAccordion(metrics) {
+    const accordion = $('#topics_accordion');
+
+    for (const [offset_name, values] of Object.entries(metrics)) {
+        let label = LABELS[values.name] || values.name;
+        if (label != values.name) {
+            label += ` (${values.name})`;
+        }
+        let progress = values.processed_precent.toFixed(2);
+        let total = getNumberWithCommas(values.total);
+        let remaining = getNumberWithCommas(values.queued);
+        let processed = getNumberWithCommas(values.processed);
+        let load_speed = values.current_load_speed.toFixed(2);
+        let processing_speed = values.current_processing_speed.toFixed(2);
+        let status = values.status;
+        let started = formatDateToYYYYMMDDHHMM(new Date(values.started));
+
+        let finishes;
+        if (values.finishes == "inf") {
+            finishes = '\u221E';
+        } else {
+            finishes = formatDateToYYYYMMDDHHMM(new Date(values.finishes));
+        }
+
+        let time_left = values.time_left;
+        if (typeof time_left == 'string') {
+            time_left = '\u221E';
+        } else {
+            time_left = formatSecondsToTimeLeft(values.time_left.toFixed(0));
+        }
+
+        let status_icon;
+        switch (status) {
+            case 'active':
+                status_icon = '<i class="bi bi-caret-right-square pe-2" style="font-size: 1rem; color:#407eff;"></i>';
+                break;
+            case 'done':
+                status_icon = '<i class="bi bi-check-square pe-2" style="font-size: 1rem; color:lightgreen;"></i>';
+                break;
+            case 'dead':    
+                status_icon = '<i class="bi bi-dash-square pe-2" style="font-size: 1rem; color:tomato;"></i>';
+        }
+
+        $(`.accordion-item.${offset_name} .accordion-button`).removeClass('active done dead');
+        $(`.accordion-item.${offset_name} .accordion-button`).addClass(status);
+        $(`.accordion-item.${offset_name} .accordion-button`).html(`${status_icon} ${label} | ${progress}%`);
+        $(`.accordion-item.${offset_name} .topic-tasks-total`).text(total);
+        $(`.accordion-item.${offset_name} .topic-tasks-processed`).text(processed);
+        $(`.accordion-item.${offset_name} .topic-tasks-queued`).text(remaining);
+        $(`.accordion-item.${offset_name} .topic-speed-load`).text(load_speed);
+        $(`.accordion-item.${offset_name} .topic-speed-processing`).text(processing_speed);
+        $(`.accordion-item.${offset_name} .topic-progress`).text(progress);
+        $(`.accordion-item.${offset_name} .topic-lime-left`).text(time_left);
+        $(`.accordion-item.${offset_name} .topic-time-started`).text(started);
+        $(`.accordion-item.${offset_name} .topic-time-finishes`).text(finishes);
+
+        refreshGraphTasksData(offset_name, values.full_tasks_graphs);
+        refreshGraphProgressData(offset_name, progress);
+    }
+        
+}
+
+
 function loadData() {
     $.ajax({
         type: 'GET',
@@ -294,23 +378,23 @@ function loadData() {
 }
 
 
-// function refreshData() {
-//     $.ajax({
-//         type: 'GET',
-//         url: 'http://localhost:8000/metrics/dashboard',
-//         success: (dashboard) => {
-//             fillTotals(dashboard.totals);
-//             fillTopicsAccordion(dashboard.metrics);
-//         },
-//         error: (res) => {
-//             //
-//         },
-//         timeout: 3000
-//     });
-// }
+function refreshData() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8000/metrics/dashboard',
+        success: (dashboard) => {
+            fillTotals(dashboard.totals);
+            fillTopicsAccordion(dashboard.metrics);
+        },
+        error: (res) => {
+            //
+        },
+        timeout: 3000
+    });
+}
 
 
 window.onload = () => {
     loadData();
-    // setInterval(refreshData, 10000);
+    setInterval(refreshData, 15000);
 };
